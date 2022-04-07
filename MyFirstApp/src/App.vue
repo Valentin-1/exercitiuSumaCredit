f6f7a5cc608585399d33d1ac077d7236ada16662
Valentin-1 <valikv03@gmail.com>
2022-04-07 13:48:30 +0300

    rezolvat

---
+++ MyFirstApp/src/App.vue
@@ -0,0 +1,71 @@
+<script>
+import HomeView  from './views/HomeView.vue';
+
+export default{
+  components: {HomeView}
+}
+
+</script>
+
+<template>
+  <div>
+    <HomeView />
+  </div>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ MyFirstApp/src/views/HomeView.vue
@@ -0,0 +1,86 @@
+<script>
+export default{
+  data(){
+    return{
+      inpSuma: "",
+      inpLuna: "",
+      sumLun: 0,
+      restSum: 0,
+     resultList: [],
+    }
+  },
+  methods: {
+    calcCredit(){
+      this.sumLun = Math.ceil(this.inpSuma / this.inpLuna);
+      this.restSum = this.inpSuma - this.sumLun;
+      
+      this.objResult();
+      console.log(this.resultList)
+    },
+    objResult(){
+      this.resultList = [];
+      for(let i = 0; i < this.inpLuna; i++){
+        //if(i == this.resultList){
+          //this.resultList.push({id: i+1, sumCurent: this.sumLun, rest: 0})
+       // }else{
+        this.resultList.push({id: i+1, sumCurent: this.sumLun, rest: this.restSum})
+        this.restSum -= this.sumLun;
+        //this.restSum = this.inpSuma - this.sumLun * (i+1);
+        //}
+      }
+    }
+  },
+}
+
+</script>
+
+<template>
+  <div>
+    <input type="number" id="Suma" v-model="inpSuma"  placeholder="Suma"> <br>
+    <input type="number" id="Data" v-model="inpLuna"  placeholder="Data">
+    <br>  
+    <button id="add" @click="calcCredit">Calculeaza</button>
+    <div id="result" v-show="resultList.length != 0" >
+      <table id="myTable">
+        <tr>
+          <th class="info">Luna</th>
+          <th class="info">Suma pe luna</th>
+          <th class="info"> Suma ramasa</th>
+        </tr>
+
+        <tr v-for="item in resultList" :key="item.id" class="info-2">
+          <td class="info-2">{{item.id}}</td>
+          <td class="info-2">{{item.sumCurent}}</td>
+          <td class="info-2">{{item.rest}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+@import "@/assets/base.css";
+
+#result {
+  margin-top: 50px;
+}
+
+#myTable{
+  border: 1px solid black;
+}
+
+.info{
+  border: 1px solid black;
+  font-weight: bold;
+  text-align: center;
+  width: 120px;
+}
+
+.info-2{
+  border: 1px solid black;
+  text-align: center;
+  width: 120px;
+}
+
+</style>
